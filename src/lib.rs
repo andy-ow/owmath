f7f139d2ce4c30343fa152b1ead2577af56cb01f
Andrzej Owsikowski <ajoinfinity@gmail.com>
2021-08-29 07:13:40 +0200

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub use self::quaternion::Quaternion;
 
 #[cfg(not(tarpaulin_include))]
 #[allow(dead_code)]
+
 fn lib() {
     let a = Quaternion::new(1.0, 2.0, 3.0, 4.0);
     let b = Quaternion::new(1.0, 2.0, 3.0, 4.0);
